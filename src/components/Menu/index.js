ec0b3c7a6e3bf4ac9a1dac866ae87b183ca56579
Gabriel <isporck0@gmail.com>
2020-08-03 22:56:29 -0300

    aula 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from  'react-router-dom'
 import Logo from '../../assets/img/logo.png';
 import './menu.css'
 import Buttton from '../Button'
@@ -7,10 +8,10 @@ import Buttton from '../Button'
 function Menu() {
     return (
         <nav className="Menu">
-            <a>
+            <Link to="/" >
              <img className="Logo" src={Logo} alt="projectflix logo"/>
-            </a>
-            <Buttton as='a' className="ButtonLink" href="/">
+            </Link>
+            <Buttton as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Buttton>
         </nav>
